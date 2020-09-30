a5b9d6be8f70be9aa7bf08fea2afbb3119ed05d6
manh <manh.vv.htth@gmail.com>
2020-10-01 01:24:39 +0700

    feat: :sparkles: login with can-pass

--- index.js
+++ index.js
@@ -1,81 +1,46 @@
 const express = require('express');
-const passport = require('passport');
-const OAuth2Strategy = require('passport-oauth2').Strategy;
 const session = require('cookie-session');
-const request = require('request');
+const CANpass = require('can-pass-verify').default;
 
+const PORT = 3002;
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(session({ name: 'sign-transaction-sample', keys: ['key'], maxAge: 24 * 60 * 60 * 1000 }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static('lib'));
 
-app.get('/', (req, res) => res.render('index', { user: req.session.passport.user }));
-
-app.post('/logout', (req, res) => {
-  delete req.session.passport;
-  res.redirect('/');
-});
-
-const PORT = 3002;
+app.get('/', (req, res) => res.render('index', { token: req.session.token }));
 
 // CANpass integration
-const clientID = '514744fe2356ecd585789ebddfecb087';
-const clientSecret = '6b88c8e215cb1c62deb07c7e176fbb1b66799705246db502127d2dd92cc7bff5';
-const callbackURL = `http://localhost:${PORT}/auth/cryptobadge/callback`;
+const client_id = '514744fe2356ecd585789ebddfecb087';
+const client_secret = '6b88c8e215cb1c62deb07c7e176fbb1b66799705246db502127d2dd92cc7bff5';
+const redirect_uri = `http://localhost:${PORT}/auth/cryptobadge/callback`;
 
-passport.use(
-  new OAuth2Strategy(
+CANpass.config({
+  canPassApi: 'https://test.canpass.me/oauth2',
+  fetch: require('node-fetch'),
+});
+
+app.get('/auth/cryptobadge/callback', (req, res) => {
+  CANpass.getToken(
     {
-      authorizationURL: 'https://test.canpass.me/oauth2/authorize',
-      tokenURL: 'https://test.canpass.me/oauth2/token',
-      state: true,
-      clientID,
-      clientSecret,
-      callbackURL,
+      code: req.query.code,
+      redirect_uri,
     },
-    (accessToken, refreshToken, params, profile, done) => {
-      console.info('accessToken', accessToken);
-
-      request.post(
-        {
-          url: 'https://dev.api.cryptobadge.app/m1/graphql',
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-          json: {
-            query: `query {
-              me {
-                id
-                name
-                email
-                path
-                resourceUrl
-              }
-            }`,
-          },
-        },
-        (error, response, { data: { me } }) => {
-          if (error || response.statusCode !== 200) {
-            console.error('error', error);
-            return done(error);
-          }
-          done(null, me);
-        },
-      );
+    {
+      client_id,
+      client_secret,
     },
-  ),
-);
+  ).then((token) => {
+    console.log('-- retrieve token:', token);
+    req.session.token = token;
+    res.redirect('/');
+  });
+});
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
-
-app.get('/auth/cryptobadge', passport.authenticate('oauth2', { scope: ['email'] }));
-app.get('/auth/cryptobadge/callback', passport.authenticate('oauth2'), (req, res) => {
-  console.info('query.code', res.query.code);
+app.get('/logout', (req, res) => {
+  delete req.session.token;
   res.redirect('/');
 });
 
